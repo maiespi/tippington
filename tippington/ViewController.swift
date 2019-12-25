4ea03adf9e24d64c0de8c42421e74bf0353f83d0
Maireen Espiritu <imaireen@gmail.com>
2019-12-24 16:49:58 -0800

    Initial Commit

---
+++ tippington/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippington
+//
+//  Created by Maireen Espiritu on 12/24/19.
+//  Copyright © 2019 Maireen Espiritu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
